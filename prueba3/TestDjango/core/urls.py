d583dc13409778e791eeb14b388f698cc8a4ca61
angelomaluenda <ang.maluenda@duocuc.cl>
2022-06-01 13:50:39 -0400

    subiendo el core

---
+++ prueba3/TestDjango/core/urls.py
@@ -0,0 +1,9 @@
+from django import views
+from django.urls import path
+from .views import index, registro
+
+
+urlpatterns = [
+    path('', index,name="index"),
+    path('registro/', registro, name="registro"),
+]
