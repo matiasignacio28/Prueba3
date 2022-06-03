1277bb52a1162061ba0d656cdf5572a332b10e58
angelomaluenda <ang.maluenda@duocuc.cl>
2022-06-03 18:41:17 -0400

    validaciones 4

--- prueba3/Prueba3/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Producto, Contacto
-
-# Register your models here.
-#Permite administrar  el modelo completo
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Contacto)
