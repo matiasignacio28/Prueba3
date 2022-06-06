d7dd7cb02c0d34450d93dad14d9104429e841f2a
angelomaluenda <ang.maluenda@duocuc.cl>
2022-06-06 14:52:20 -0400

    validaciones +

--- prueba3/TestDjango/core/forms.py
+++ prueba3/TestDjango/core/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from .models import Categoria, Producto, Contacto
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ValidationError
 
 
 class ContactoForm(forms.ModelForm):
-    
+    nombre = forms.CharField(min_length=3, max_length=50)
     class Meta:
         model = Contacto
         fields = '__all__'
@@ -20,7 +21,11 @@ class CustomUserCreationForm(UserCreationForm):
     
 class ProductoForm(forms.ModelForm):
 
+    nombreProducto = forms.CharField(min_length=3, max_length=80)
     imagen = forms.ImageField(required=False)
+    precio = forms.IntegerField(min_value=1, max_value=1500000)
+
+
 
     class Meta:
         model = Producto
